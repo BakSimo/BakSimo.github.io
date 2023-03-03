9f062243729f0b4466f82e510c44010f5000a89c
BakSimo <tigrankarapait@gmail.com>
2023-03-03 11:37:02 +0400

    connect, errors

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,32 +1,51 @@
-import React from "react";
+// import React from "react";
+// import StoreContext from "../../../StoreContext";
+import { connect } from "react-redux";
 import { updateNewPostTextAC, addPostAC } from "../../../redux/profile-reducer";
-import StoreContext from "../../../StoreContext";
 import MyPosts from "./MyPosts";
 
-const MyPostsContainer = () => {
-  return (
-    <StoreContext.Consumer>
-      {(store) => {
-        let state = store.getState();
-        let addPost = () => {
-          store.dispatch(addPostAC());
-        };
+// const MyPostsContainer = () => {
+//   return (
+//     <StoreContext.Consumer>
+//       {(store) => {
+//         let state = store.getState();
+//         let addPost = () => {
+//           store.dispatch(addPostAC());
+//         };
 
-        let onPostChange = (text) => {
-          let action = updateNewPostTextAC(text);
-          store.dispatch(action);
-        };
-        return (
-          <MyPosts
-            updateNewPostText={onPostChange}
-            addPost={addPost}
-            posts={state.profilePage.posts}
-            newPostText={state.profilePage.newPostText}
-          />
-        );
-      }}
-    </StoreContext.Consumer>
-  );
+//         let onPostChange = (text) => {
+//           let action = updateNewPostTextAC(text);
+//           store.dispatch(action);
+//         };
+//         return (
+//           <MyPosts
+//             updateNewPostText={onPostChange}
+//             addPost={addPost}
+//             posts={state.profilePage.posts}
+//             newPostText={state.profilePage.newPostText}
+//           />
+//         );
+//       }}
+//     </StoreContext.Consumer>
+//   );
+// };
+
+let mapStateToProps = (state) => {
+  return {
+    posts: state.profilePage.posts,
+    newPostText: state.profilePage.newPostText,
+  };
 };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateNewPostText: (text) => {
+      let action = updateNewPostTextAC(text);
+      dispatch(action);
+    },
+    addPost: () => {dispatch(addPostAC());},
+  };
+};
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 
 export default MyPostsContainer;
